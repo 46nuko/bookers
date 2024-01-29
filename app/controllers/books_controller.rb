1e0d40e39bd5cd7c11b56bb599d94f9b83723650
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-01-29 13:29:24 +0000

    CRUDのアクションにリンク作成とtopページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,26 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
 
   def index
     @books = Book.all
+    @book = Book.new
+
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   def show
@@ -24,14 +35,11 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-  end
+
 
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'books#new'
+  root to: 'homes#top'
   get 'top' => 'homes#top'
   resources :books
   end
