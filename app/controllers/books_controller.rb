15a23285028b45439430ef5fb11c85e0dee35eac
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-01-31 12:46:34 +0000

    bookers完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-
   end
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created."
+      flash[:error] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -33,15 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.new(book_params)
-    if @book.save
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       redirect_to book_path(@book.id)
+      flash[:error]="Book was successfully updated"
     else
-      render :new
+      render :edit
     end
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
   end
 
 
